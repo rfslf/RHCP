93b7df83adf476a0c735ff7b69a57294f926d500
Авдонин Станислав Михайлович <avdonin@unn.global>
2019-12-25 17:10:57 +0300

    Version 0.1 of my WoW addon RaidHealersEasyLife

--- RHEL.lua
+++ RHEL.lua
@@ -1,7 +1,7 @@
 -- Author      : Virgo
 -- Create Date : 12/19/2019 7:43:57 PM
 
-RHEL_Heals={}
+-- RHEL_Heals={}
 
 SLASH_RHEL_SLASHCMD1 = '/RHEL'
 SLASH_RHEL_SLASHCMD2 = '/rhel'
@@ -18,56 +18,80 @@ function RHEL_OnMouseUp()
 	RHEL_MainFrame:StopMovingOrSizing()
 end
 
-function RHEL_OnLoad()
+function RHEL_HealsOnLoad()
 	print('Start')
 	if RHEL_Heals == nil then
-   		RHEL_Heals = {true,false,false,false,false,false,false,false,false,false,false,false}; -- This is the first time this addon is loaded; initialize heals to false.
+   		RHEL_Heals = {{true,false,false,false,false,false,false,false,false,false,false,false},{}}; -- This is the first time this addon is loaded; initialize heals to false.
   	end
-	CheckButton11_1:SetChecked(RHEL_Heals[1]);
-	CheckButton11_2:SetChecked(RHEL_Heals[2]);
-	CheckButton11_3:SetChecked(RHEL_Heals[3]);
-	CheckButton11_4:SetChecked(RHEL_Heals[4]);
-	CheckButton11_5:SetChecked(RHEL_Heals[5]);
-	CheckButton11_6:SetChecked(RHEL_Heals[6]);
-	CheckButton11_7:SetChecked(RHEL_Heals[7]);
-	CheckButton11_8:SetChecked(RHEL_Heals[8]);
-	CheckButton11_9:SetChecked(RHEL_Heals[9]);
-	CheckButton11_10:SetChecked(RHEL_Heals[10]);
-	CheckButton11_11:SetChecked(RHEL_Heals[11]);
-	CheckButton11_12:SetChecked(RHEL_Heals[12]);
---  _G['CheckButton11_'..number]:SetChecked(RHEL_Heals[number]);
+--	CheckButton11_1:SetChecked(RHEL_Heals[1]);
+	for i = 1, 2 do
+		for j = 1, 12 do
+			_G['CheckButton1' .. i .. "_"..j]:SetChecked(RHEL_Heals[i][j]);
+		end
+	end
 	print('Finish')
 end
 
 function RHEL_HealAnounce()
-	local message=""
-	for i = 1, 12 do
-		if RHEL_Heals[i] then
-			if message == "" then
-				message = "HEALING:" .. "Group " .. i
-			else
-				message = message .. ",Group " .. i
-			end
-		else
-			message = "None"
+	local anounce = "HEALING: "
+	for i = 1,2 do
+		local message
+		if __G['HealerName'..i]:GetText() == nil
+			break
+		if message == "" then
+			message = "[" .. __G['HealerName'..i]:GetText() .. " -"
 		end
-	print(message)
+		if RHEL_Heals then
+			for j = 1, 8 do
+				if RHEL_Heals[j] then
+					message = message .. " Group" .. j
+				end
+			end
+			if RHEL_Heals[9] then
+				message = message .. " MT"
+			end
+			for j = 10, 12 do
+				if RHEL_Heals[j] then
+					message = message .. " T" .. (j-8)
+				end
+			end
+		end
+		anounce = anounce .. message .. "]"
+	end
+	print(anounce)
 end
 
-function ClickOnHealCheckBox(number)
+function ClickOnHealCheckBox(healer,target)
 	local isChecked
-	isChecked = CheckButton11_1:GetChecked()
---  isChecked=_G['CheckButton11_'..number]:GetChecked()
-	CheckButton11_1:SetChecked(not isChecked)
---	_G['CheckButton11_'..number]:SetChecked(not isChecked)
+--	isChecked = CheckButton11_1:GetChecked()
+    isChecked=_G['CheckButton1' .. healer.. '_'..number]:GetChecked()
+--	CheckButton11_1:SetChecked(not isChecked)
+	_G['CheckButton1' .. healer.. '_'..number]:SetChecked(not isChecked)
 	RHEL_Heals[number] = not isChecked
 end
 
 function HealerNameChange(healer)
-	local id = tonumber(string.sub(healer:GetName(),11))
+-- for example
+-- healer:GetName() get HealerName1
+-- string.sub(healer:GetName(),11) take from 11-th character inclusive and get '1'
+-- tonumber turns variable to 1
+	local id = tonumber(string.sub(healer:GetName(),11)) 
 	UpdateHealerClass(id);
 end
 
+-- TO DO
+function UpdateHealerClass(icon)
+	local _,englishClass = UnitClass(_G["HealerName"..icon]:GetText());
+	if englishClass ~= nil then
+		_G["HealerClass"..icon.."Texture"]:SetTexture("Interface\\Addons\\RHEL\\Icons\\"..englishClass);
+		_G["HealerClass"..icon.."DragTexture"]:SetTexture("Interface\\Addons\\RHEL\\Icons\\"..englishClass);
+	else
+		_G["HealerClass"..icon.."Texture"]:SetTexture(nil);
+		_G["HealerClass"..icon.."DragTexture"]:SetTexture(nil);
+	end
+end
+
+
 function EditBox_OnEscapePressed()
-	SurgeonGeneralConfigFrame:Hide()
+	RHEL_MainFrame:Hide()
 end
