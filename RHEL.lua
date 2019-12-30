86a5532ed4f8597bb233f945f2007826582b783a
Авдонин Станислав Михайлович <avdonin@unn.global>
2019-12-30 14:25:50 +0300

    Version 0.4. SavedVariables, to healer wisper, radiobuttons, frame about

--- RHEL.lua
+++ RHEL.lua
@@ -1,80 +1,94 @@
 -- Author      : Virgo
 -- Create Date : 12/19/2019 7:43:57 PM
 
-local version = 0.3
+local version = 0.4
 local totalHealers = 8
 
-local RaidNameList = {"Molten Core","Onyxia's Lair & Outdoors","Blackwing Lair","Naxxramas", "Custome"};
+local RaidNameList = {"Molten Core","Onyxia & Outdoors","Blackwing Lair","Naxxramas", "Custome"};
 local BossNameList = {};
-BossNameList.Onyxia = {"Onyxia","Azuregos","Kazzak"};
 BossNameList.MC = {"Trash","Lucifron","Magmadar","Gehennas","Garr","Baron Geddon",
 				   "Shazzrah","Golemagg","Sulfuron","Majordomo","Ragnaros"};
+BossNameList.Onyxia = {"Onyxia","Azuregos","Kazzak"};
 BossNameList.BWL = {"Trash","Razorgore","Vaelastrasz","Broodlord","Firemaw",
 					"Ebonroc","Flamegor","Chromaggus","Nefarian"}   
 BossNameList.NAX = {"Trash","Anub'Rekhan","Grand Widow Faerlina","Maexxna",
 					"Noth the Plaguebringer","Heigan the Unclean","Loatheb",
 					"Instructor Razuvious","Gothic the Harvester","The Four Horsemen",
 					"Patchwerk","Grobbulus","Gluth","Thaddius","Sapphiron","Kel'Thuzad"};
+BossNameList.Custome = {"Frame_1","Frame_2","Frame_3","Frame_4"};
 				   
-local VariablesLoaded
 
+--Add command line reaction. DONE
 SLASH_RHEL_SLASHCMD1 = '/RHEL'
 SLASH_RHEL_SLASHCMD2 = '/rhel'
 SlashCmdList["RHEL_SLASHCMD"] = function(msg)
     RHEL_MainFrame:Show()
 end
 
+--Variables loading detection. CHECK
+local VariablesLoaded
+
+RHEL_MainFrame:RegisterEvent("ADDON_LOADED")
+RHEL_MainFrame:SetScript("OnEvent", function(self,event,addon)
+	if eveny == "ADDON_LOADED" and addon == "RHEL" then
+		print("Varaibles loaded")
+		VariablesLoaded = true
+		RHEL_VariablesLoaded()
+	end
+end)
+
+--Greetings. DONE
 function RHEL_Loaded()
---CHECK
-	print('RaidHealersEasyLife loaded. Type /rhel for menu. Version '..version);
+	print('RaidHealersEasyLife loaded. Type /rhel to open menu. Version '..version);
 --	RHEL_MainFrame:Hide();
 end
 
+--Frame starts moving. DONE
 function RHEL_OnMouseDown()
---DONE
 	RHEL_MainFrame:StartMoving()
 end
 
+--Frame stops moving. DONE
 function RHEL_OnMouseUp()
---DONE
 	RHEL_MainFrame:StopMovingOrSizing()
 end
 
-function RHEL_VariablesLoaded(event)
---TO DO
-	if(event == "VARIABLES_LOADED") then
-		if RHEL_Heals == nil then
-			RHEL_Heals = {{false,false,false,false,false,false,false,false,true,false,false,false},
-						  {false,false,false,false,false,false,false,false,true,false,false,false},
-						  {false,false,false,false,false,false,false,false,true,true,false,false},
-						  {false,false,false,false,false,false,false,false,false,true,false,false},
-						  {true,false,true,false,true,false,true,false,true,false,true,false},
-						  {true,false,true,false,true,false,true,false,true,false,true,false},
-						  {false,true,false,true,false,true,false,true,false,true,false,true},
-						  {false,true,false,true,false,true,false,true,false,true,false,true}}; 
---							  1	   2    3     4    5     6    7     8    MT    T2	T3	  T4
--- This is the first time this addon is loaded; initialize heals to defaults.
-		end
-		if RHEL_Healers == nil then
-			for i, totalHealers do
-				RHEL_Healers[i] = "Name"..i;
--- This is the first time this addon is loaded; initialize healers to NameNumber.
-		end
-		RHEL_HealersLoad();
-		RHEL_HealsLoad();
-		VariablesLoaded = true
+--Defaults values to variables or load saved. TO DO
+local login = true
+
+function RHEL_VariablesLoaded()
+-- For the first time RHEL_Heals is loaded; initialize heals to defaults.
+	if RHEL_Heals == nil then 
+--						 1	   2     3     4     5     6     7     8    MT    T2	T3	  T4
+		RHEL_Heals = {{false,false,false,false,false,false,false,false,true,false,false,false},
+					  {false,false,false,false,false,false,false,false,true,false,false,false},
+					  {false,false,false,false,false,false,false,false,true,true,false,false},
+					  {false,false,false,false,false,false,false,false,false,true,false,false},
+					  {true,false,true,false,true,false,true,false,true,false,true,false},
+					  {true,false,true,false,true,false,true,false,true,false,true,false},
+					  {false,true,false,true,false,true,false,true,false,true,false,true},
+					  {false,true,false,true,false,true,false,true,false,true,false,true}};
+--						 1	   2    3     4    5     6    7     8    MT    T2	T3	  T4					  
 	end
+-- For first time RHEL_Healers is loaded; initialize healers to NameNumber.
+	if RHEL_Healers == nil then 
+		for i=1, totalHealers do
+			RHEL_Healers[i] = "Name"..i;
+		end 
+	end
+	RHEL_HealersLoad();
+	RHEL_HealsLoad();
 end
 
+--Load healers on load. CHECK
 function RHEL_HealersLoad()
---CHECK
 	for i = 1, totalHealers do
-		_G['HealerName'..i]:SetText(RHEL_Healers[i])
+		_G['HealerName'..i]:SetText(RHEL_Healers[i]);
 	end
 end
 
+--Load heals checkboxes on load. Optimaze
 function RHEL_HealsLoad()
---Optimaze
 	for i = 1, totalHealers do
 		for j = 1, 12 do
 			if RHEL_Heals[i][j] then
@@ -86,54 +100,88 @@ function RHEL_HealsLoad()
 	end
 end
 
+--Click on heals anounce. Add skip empty healer
 function RHEL_HealAnounce()
---DONE
 	local anounce = "HEALINGS: "
 	for i = 1,totalHealers do
-		local message = "[" .. _G['HealerName'..i]:GetText() .. " -"
-
-		if RHEL_Heals[i] then
-			for j = 1, 12 do
-				if RHEL_Heals[i][j] then
-					if j < 9 then
-						message = message .. " Group" .. j
-					elseif j == 9 then
-						message = message .. " MT"
-					elseif j > 9 then
-						message = message .. " T" .. (j-8)
+		if _G['HealerName'..i]:GetText() ~= nil then
+			local message1 = "[" .. _G['HealerName'..i]:GetText() .. " -"
+			local message2 = ""
+			if RHEL_Heals[i] then
+				for j = 1, 12 do
+					if RHEL_Heals[i][j] then
+						if j < 9 then
+							message2 = message2 .. " Group" .. j
+						elseif j == 9 then
+							message2 = message2 .. " MT"
+						elseif j > 9 then
+							message2 = message2 .. " T" .. (j-8)
+						end
 					end
 				end
 			end
-		end	
-		anounce = anounce .. message .. "]  "
+			if message2 ~= "" then
+				anounce = anounce .. message1 .. message2 .. "]  "	
+			end
+		end
 	end
 	print(anounce)
 end
 
-function ClickOnHealCheckBox(healer,target)
---CHECK
-	local isChecked
---	isChecked = CheckButton11_1:GetChecked()
-    isChecked=_G['CheckButton1' .. healer.. '_'..target]:GetChecked();
---	CheckButton11_1:SetChecked(not isChecked)
---	_G['CheckButton1' .. healer.. '_'..target]:SetChecked(not isChecked);
-	RHEL_Heals[healer][target] = isChecked
-	print(RHEL_Heals[healer][target])
+--Click on buffs anounce. TO DO
+function RHEL_BuffAnounce()
+	print("Buffs")
 end
 
-function HealerNameChange(healer)
---CHECK
+--Click on buffs anounce. TO DO
+function RHEL_DispellAnounce()
+	print("Dispells")
+end
+
+--Click on healers personal anounce. Add skiping on empty healer
+function RHEL_HealerWisper(number);
+	local healer = _G['HealerName'..number]:GetText()
+	local wisper = ""
+	if healer ~= nil then
+		wisper = healer .. ": "
+		if RHEL_Heals[number] then
+			for j = 1, 12 do
+				if RHEL_Heals[number][j] then
+					if j < 9 then
+						wisper = wisper .. " Group" .. j
+					elseif j == 9 then
+						wisper = wisper .. " MT"
+					elseif j > 9 then
+						wisper = wisper .. " T" .. (j-8)
+					end
+				end
+			end
+		end
+		print(wisper)
+	end
+end
+
+--Click on checkbox reaction. CHECK
+function ClickOnHealCheckBox(healer,target)
+	local isChecked
+    isChecked=_G['CheckButton1' .. healer.. '_'..target]:GetChecked();
+	RHEL_Heals[healer][target] = isChecked
+--	print(RHEL_Heals[healer][target])
+end
+
+--Healer name editing reaction. CHECK
 -- for example
 -- healer:GetName() get HealerName1
 -- string.sub(healer:GetName(),11) take from 11-th character inclusive and get '1'
 -- tonumber turns variable to 1
+function HealerNameChange(healer)
 	local id = tonumber(string.sub(healer:GetName(),11))
 	UpdateHealerClass(id);
 	RHEL_Healers[id] = _G['HealerName'..id]:GetText()
 end
 
+--Select icon for healer. CHECK
 function UpdateHealerClass(icon)
---CHECK
 	local localizedClass, englishClass, classIndex =  UnitClass(_G["HealerName"..icon]:GetText());
 	if englishClass ~= nil then
 		_G["HealerClass"..icon.."Texture"]:SetTexture("Interface\\Addons\\RHEL\\Icons\\"..englishClass);
@@ -144,17 +192,25 @@ function UpdateHealerClass(icon)
 	end
 end
 
+--Removes focus from healer name window. CHECK
 function EditBox_OnEscapePressed(number)
---CHECK
-	_G["HealerName"..number]:SetAutoFocus(false)
+	_G["HealerName"..number]:ClearFocus()
 --	RHEL_MainFrame:Hide()
 end
 
+--Swap chat to anounce. TO DO
+function SwapAnounceTo()
+	if toChannel:GetChecked then
+		toRaid:SetChecked(false)
+	else
+		toRaid:SetChecked(true)
+	end
+end
 
+--RaidName menu. CHECK
 local info = {};
 
 function RaidNameDropdown_OnLoad()
---TO DO
 	if (VariablesLoaded == false) then
 		return;
 	end;
@@ -171,8 +227,8 @@ function RaidNameDropdown_OnLoad()
 	end
 end
 
+--Set raid name on select. CHECK
 function RaidName_OnSelect(value)
---TO DO
 	if (UIDropDownMenu_GetSelectedValue(_G["RaidNameDropdown"]) ~= value) then
 		UIDropDownMenu_SetSelectedValue(_G["RaidNameDropdown"], value);
 		UIDropDownMenu_ClearAll(_G["BossNameDropdown"]);
@@ -180,8 +236,8 @@ function RaidName_OnSelect(value)
 	end
 end
 
+--BossName menu. CHECK
 function BossNameDropdown_OnLoad()
---TO DO
 	if (VariablesLoaded == false) then
 		return;
 	end;
@@ -190,7 +246,7 @@ function BossNameDropdown_OnLoad()
 	local List = {};
 	
 	if UIDropDownMenu_GetSelectedValue(_G["RaidNameDropdown"]) ~= nil then
-		List = select(UIDropDownMenu_GetSelectedValue(_G["RaidNameDropdown"]),BossNameList.KZ, BossNameList.GL, BossNameList.ML, BossNameList.SSC, BossNameList.TK, BossNameList.MH, BossNameList.BT, BossNameList.SWP, BossNameList.NAX, BossNameList.OS, BossNameList.EOE, BossNameList.VoA, BossNameList.Ulduar, BossNameList.TotC, BossNameList.TotGC, BossNameList.Onyxia, BossNameList.IC, BossNameList.RS, BossNameList.BH, BossNameList.BoT, BossNameList.BWD, BossNameList.TotFW, BossNameList.Firelands, BossNameList.DragonSoul);
+		List = select(UIDropDownMenu_GetSelectedValue(_G["RaidNameDropdown"]),BossNameList.MC, BossNameList.Onyxia, BossNameList.BWL, BossNameList.NAX, BossNameList.Custome);
 	end
 
 	for x=1,getn(List) do
@@ -207,13 +263,13 @@ function BossNameDropdown_OnLoad()
 		UIDropDownMenu_SetSelectedValue(_G["BossNameDropdown"],1)
 		UIDropDownMenu_SetText(_G["BossNameDropdown"],List[1])
 		if _G["RHEL_MainFrame"]:IsShown() then
-			RHEL_MainFrame:LoadFromFile(UIDropDownMenu_GetText(_G["RaidNameDropdown"]).." - "..UIDropDownMenu_GetText(_G["BossNameDropdown"]));
+			print(UIDropDownMenu_GetText(_G["RaidNameDropdown"]).." - "..UIDropDownMenu_GetText(_G["BossNameDropdown"]));
 		end
 	end
 end
 
+--Set boss name on select. CHECK
 function BossName_OnSelect(value)
---TO DO
 	UIDropDownMenu_SetSelectedValue(_G["BossNameDropdown"], value);
-	RHEL_MainFrame:LoadFromFile(UIDropDownMenu_GetText(_G["RaidNameDropdown"]).." - "..UIDropDownMenu_GetText(_G["BossNameDropdown"]));
+	print(UIDropDownMenu_GetText(_G["RaidNameDropdown"]).." - "..UIDropDownMenu_GetText(_G["BossNameDropdown"]));
 end
